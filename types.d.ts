ba5be7586c6dffe1e4fb258fed034a72c4368a70
catamphetamine <kuchumovn@gmail.com>
2021-09-10 06:53:46 +0300

    #45. Metadata TypeScript export

--- types.d.ts
+++ types.d.ts
@@ -20,6 +20,10 @@ export type Countries = {
   //   possible_lengths: number[],
   //   ...
   // }
+  //
+  // `in` operator docs:
+  // https://www.typescriptlang.org/docs/handbook/advanced-types.html#mapped-types
+  // `country in CountryCode` means "for each and every CountryCode".
   [country in CountryCode]: any[];
 };
 
