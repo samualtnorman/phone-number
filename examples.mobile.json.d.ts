a04842622b81ea1c545c2fe0f221ae7602cdc72a
catamphetamine <kuchumovn@gmail.com>
2021-09-10 11:50:40 +0300

    #45. Fixed example numbers TypeScript

--- examples.mobile.json.d.ts
+++ examples.mobile.json.d.ts
@@ -1,8 +1,8 @@
-import { CountryCode, E164Number } from './types';
+import { CountryCode, NationalNumber } from './types';
 
 export default interface Examples {
   // `in` operator docs:
   // https://www.typescriptlang.org/docs/handbook/advanced-types.html#mapped-types
   // `country in CountryCode` means "for each and every CountryCode".
-  [country in CountryCode]: E164Number;
+  [country in CountryCode]: NationalNumber;
 };
